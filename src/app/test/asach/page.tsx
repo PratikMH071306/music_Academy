8d5146830a8a0b78187744288358da3d6d43a477
PratikMH071306 <pratikraorane65@gmail.com>
2025-07-06 23:55:53 +0530

    Setting up Navbar using Acertinity UI.

--- src/app/test/asach/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function asachpage() {
-  return <h1>Asach Page</h1>;
-}
--- src/app/test/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function testpage() {
-  return <h1 className="text-3xl text-center mt-8">Test Page</h1>;
-}
