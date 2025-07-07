5f5f1d8e5ef46dbf6b76cf57ea81fbe16b78f172
PratikMH071306 <pratikraorane65@gmail.com>
2025-07-07 22:35:12 +0530

    Remove Unncessory part.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import HeroSection from "@/components/HeroSection";
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-gradient-white/[0.02]">
-      <h1 className="text-2xl text-center">Meta</h1>
       <HeroSection />
     </main>
   );
