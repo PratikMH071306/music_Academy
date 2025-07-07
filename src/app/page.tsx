22b4f21217984599ec014d67882f194a54a9db2d
PratikMH071306 <pratikraorane65@gmail.com>
2025-07-07 22:32:13 +0530

    Hero Section is Completed.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import HeroSection from "@/components/HeroSection";
+
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-gradient-white/[0.02]">
-      <h1 className="text-2xl text-center">Meta</h1>;
+      <h1 className="text-2xl text-center">Meta</h1>
+      <HeroSection />
     </main>
   );
 }
