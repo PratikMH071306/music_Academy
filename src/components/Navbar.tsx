163bb66376bf3bcc791a58459a093287159ce8b1
PratikMH071306 <133345320+PratikMH071306@users.noreply.github.com>
2025-07-09 01:06:22 +0530

    Update Navbar.tsx
    Solving Bug..

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,23 +1,20 @@
 "use client";
 import React, { useState } from "react";
-import Link from "next/link";
-import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
+import { HoveredLink, Menu, MenuItem } from "./ui/navbar-menu";
 import { cn } from "@/lib/utils";
 
 function Navbar({ className }: { className?: string }) {
   const [active, setActive] = useState<string | null>(null);
+
   return (
     <div
       className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
     >
       <Menu setActive={setActive}>
-        <Link href={"/"}>
-          <MenuItem
-            setActive={setActive}
-            active={active}
-            item="Home"
-          ></MenuItem>
-        </Link>
+        <MenuItem setActive={setActive} active={active} item="Home">
+          <HoveredLink href="/">Home</HoveredLink>
+        </MenuItem>
+
         <MenuItem setActive={setActive} active={active} item="Our Courses">
           <div className="flex flex-col space-y-4 text-sm">
             <HoveredLink href="/courses">All Courses</HoveredLink>
@@ -27,13 +24,10 @@ function Navbar({ className }: { className?: string }) {
             <HoveredLink href="/courses">Music Production</HoveredLink>
           </div>
         </MenuItem>
-        <Link href={"/contact"}>
-          <MenuItem
-            setActive={setActive}
-            active={active}
-            item="Contact Us"
-          ></MenuItem>
-        </Link>
+
+        <MenuItem setActive={setActive} active={active} item="Contact Us">
+          <HoveredLink href="/contact">Contact Us</HoveredLink>
+        </MenuItem>
       </Menu>
     </div>
   );
